3450706ca3907037d000a7c84a6c7cc842f68c93
Ziven <contact@ziven.me>
2025-10-28 14:11:41 +0800

    chore: update vitest configuration to include path aliases for shared and rereact packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,14 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({})
+export default defineConfig({
+  resolve: {
+    alias: {
+      'shared': path.resolve(__dirname, './packages/shared/src/index.ts'),
+      'rereact': path.resolve(__dirname, './packages/rereact/src/index.ts'),
+      'rereact-dom': path.resolve(__dirname, './packages/rereact-dom/src/index.ts'),
+      'rereact-reconciler': path.resolve(__dirname, './packages/rereact-reconciler/src/index.ts'),
+      'rereact-scheduler': path.resolve(__dirname, './packages/rereact-scheduler/src/index.ts'),
+    },
+  },
+})
