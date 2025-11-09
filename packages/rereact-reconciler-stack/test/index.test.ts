a61d514055c316d1686dbe2996a33395e5250a36
Ziven <contact@ziven.me>
2025-11-09 23:55:54 +0800

    feat: add nested elements render tests

--- packages/rereact-reconciler-stack/test/index.test.ts
+++ packages/rereact-reconciler-stack/test/index.test.ts
@@ -2,10 +2,19 @@ import { jsx } from 'rereact/jsx-runtime'
 import { describe, expect, it } from 'vitest'
 import { render } from '../src'
 
-describe('should', () => {
-  it('render empty node when children is null', () => {
+describe('stack Reconciler - DOM Component', () => {
+  it('render nested elements', () => {
     const root = document.createElement('div')
-    const element = jsx('div', { children: null })
+    const element = jsx('div', {
+      children: jsx('span', { children: null }),
+    })
+    render(element, root)
+    expect(root.innerHTML).toBe('<div><span></span></div>')
+  })
+
+  it('render empty div', () => {
+    const root = document.createElement('div')
+    const element = jsx('div', {})
     render(element, root)
     expect(root.innerHTML).toBe('<div></div>')
   })
