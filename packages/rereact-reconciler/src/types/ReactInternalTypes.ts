36f88d0ca62d733d77309bf569c64b657f706830
ziven <contact@ziven.me>
2025-12-02 19:02:36 +0800

    feat: init fiber type

---
+++ packages/rereact-reconciler/src/types/ReactInternalTypes.ts
@@ -0,0 +1,6 @@
+// https://github.com/acdlite/react-fiber-architecture?tab=readme-ov-file#structure-of-a-fiber
+// Fiber 是一个包含组件信息的对象，它对应一个栈帧，但同时也对应一个组件的实例。
+
+// Fiber 是 React 对组件渲染工作的抽象，每个组件可能对应多个 Fiber，用于描述当前和即将完成的工作状态。
+export interface Fiber {
+}
